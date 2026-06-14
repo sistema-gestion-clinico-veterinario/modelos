fde8a783fc2f7aaa50207ba08ba4a9dfc1f6f8bd
Diogho Matteo Baylon Toledo <dbaylont1@upao.edu.pe>
2026-06-13 22:50:12 -0500

    Integración de Radiografiasv2

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.responses import Response
 from fastapi.middleware.cors import CORSMiddleware
 from predictor import predictor
 from router import router
@@ -26,3 +27,8 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+def favicon():
+    return Response(status_code=204)
